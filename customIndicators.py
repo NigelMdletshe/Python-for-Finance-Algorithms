e24518f37ccb73dbe94c78fc3701009ced81096d
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-05-15 23:36:55 +0200

    added some imports

--- customIndicators.py
+++ customIndicators.py
@@ -1,1 +1,9 @@
  #backtesting Custom indicators
+# run the command below
+#pip3 backtesting pandas pandas_ta
+#create a virtual env
+
+from backtesting import Backtest, Strategy
+from backtesting.test import GOOG
+
+print(GOOG)
