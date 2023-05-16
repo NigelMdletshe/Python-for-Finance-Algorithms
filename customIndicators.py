6b560374e7b0124094236913b459807dac6797d7
Nigel Mdletshe <nigelmdletshe@gmail.com>
2023-05-16 02:02:10 +0200

    first part ends here

--- customIndicators.py
+++ customIndicators.py
@@ -14,13 +14,25 @@ print(GOOG)
 class SignalStrategy(Strategy):
     def init(self):
         pass
-    
+
+
     def next(self):
         print(self.data.Close[-1])
         print(len(self.data.Close))
+        current_signal = self.data.Signal[-1]
 
-        pass
+        if current_signal == 1:
+            if not self.position:
+                self.buy()
+            elif current_signal == -1:
+                if self.position:
+                    self.position.close()
+
+    
 
 bt = Backtest(GOOG, SignalStrategy, cash = 10_000)
 
 stats = bt.run()
+bt.plot()
+# First part to import signals and compare them to random entries end here.
+
